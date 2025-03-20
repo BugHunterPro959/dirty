55148b22c1b9719cb4616aab937210a0258f61c5
BugHunterPro959 <fajivej762@prorsd.com>
2025-03-20 19:52:45 +0000

    Optimize API - refactoring code

--- lib/cookie/domainMatch.ts
+++ lib/cookie/domainMatch.ts
@@ -53,8 +53,8 @@ export function domainMatch(
     return undefined
   }
 
-  const _str: Nullable<string>
-  const _domStr: Nullable<string>
+  let _str: Nullable<string>
+  let _domStr: Nullable<string>
 
   if (canonicalize !== false) {
     _str = canonicalDomain(domain)
